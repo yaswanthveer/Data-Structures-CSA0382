5d90b872a4a30285c500f63aab7de07097bb43f4
Yaswanth Veer <107987482+yaswanthveer@users.noreply.github.com>
2022-10-07 11:38:30 +0530

    Add files via upload

---
+++ Reverse a string DSA.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str[50], temp;
+	int i, left, right, len;
+	printf(" \n Display a reverse string in the C: \n");
+	printf("-----------------------------");
+	printf(" \n Enter a string to reverse order: ");
+	scanf("%s", &str);
+	len = strlen(str);
+	left = 0;
+	right= len - 1;
+	for (i = left; i<right ; i++)
+	{
+		temp = str[i];
+		str[i]= str[right];
+		str[right]= temp;
+		right--;
+	}
+	printf("The reverse of the oringinal string is: %s ", str);
+	getch();
+}
